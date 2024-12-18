7478ab2750195eaae5e24868e7783880245ead0d
Lilholt <63080594+mrlilholt@users.noreply.github.com>
2024-12-18 18:37:25 -0500

    fbb again

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,26 +1,20 @@
-// Import Firebase SDK
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; // For Firestore database
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDF8RFUXRMB5j9opsdgkC9do_5YaMOLPE",
+  apiKey: "AIzaSyDF8RFUXRMB5j9opsdgkC9do_5YaMOLPE", // Double-check this from Firebase
   authDomain: "mushroom-recipes-1c2eb.firebaseapp.com",
   projectId: "mushroom-recipes-1c2eb",
   storageBucket: "mushroom-recipes-1c2eb.appspot.com",
   messagingSenderId: "54616764608",
   appId: "1:54616764608:web:9b862b1049cc2de163d25",
-  measurementId: "G-K5MB451GRB",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firestore and Auth
-const db = getFirestore(app);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
 
 // Function to handle Google Sign-In (place this in your component)
 export const signInWithGoogle = async () => {

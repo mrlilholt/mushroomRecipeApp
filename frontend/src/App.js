9014f6518def583d59ddd77ac268dbd0406a58fc
Lilholt <63080594+mrlilholt@users.noreply.github.com>
2024-12-18 21:17:14 -0500

    appyladooda

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useCallback } from "react";
 import axios from "axios";
 import { Box, Card, CardMedia, Typography, Grid, Button } from "@mui/material";
 import { db } from "./firebase";

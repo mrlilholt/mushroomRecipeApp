c5140d2e258243d62eaf4ec06de79f062b3d66f6
Lilholt <63080594+mrlilholt@users.noreply.github.com>
2024-12-19 08:12:32 -0500

    firebase updates with hidden

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 // Your Firebase config using environment variables
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY, // Use environment variable
-  authDomain: "mushroom-recipes-1c2eb.firebaseapp.com",
-  projectId: "mushroom-recipes-1c2eb",
-  storageBucket: "mushroom-recipes-1c2eb.appspot.com",
-  messagingSenderId: "54616764608",
-  appId: "1:54616764608:web:9b862b1049cc2de163d25",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
